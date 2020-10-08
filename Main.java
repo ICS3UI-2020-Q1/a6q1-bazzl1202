d24aceaeca5941b91a15ffc9b417489dda3a47d1
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-08 13:53:29 +0000

    Allows user to imput stuff

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks for number of heights says average and says heights above average.
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for the number of heights
+    System.out.println("How many heights would you like to enter");
+    // create a varaible for user height input
+    int heightNumber = input.nextInt();
+
+    // ask the user to enter in all the heights
+    System.out.println("Please enter the heights on separate lines");
+
+    // create an array
+    double[] heights = new double[heightNumber];
+
+    //put heights into array using loop
+    for(int i = 0; i < heightNumber; i++){
+      int heightInput = input.nextInt();
+    }
     
   }
 }
+
